f63a6de2da4fbff4331389e7abbad14fd191ce7c
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2025-07-18 20:49:55 -0400

    remove conflicting svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [tailwind()],
   output: 'static',
   site: 'https://jenniferlynparsons.github.io',
   base: '/selfcaretech',
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
