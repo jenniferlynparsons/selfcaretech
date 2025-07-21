b0581fde2160b55006e7828c37a0eaa23f3ba10c
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2025-07-20 21:05:05 -0400

    updates to deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
   site: 'https://selfcare.tech',
-  base: '/', // Explicitly set base path for custom domain
   vite: {
     build: {
       assetsInlineLimit: 0
